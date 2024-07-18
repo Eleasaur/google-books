8fde2cae951e1f6958048599f5c32bc8679b143e
Eleasaur <eleanorbuonomo@gmail.com>
2024-07-18 22:26:05 +1000

    added search bar and books service js

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+
+const SearchBar = ({ onSubmit }) => {
+  const formRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current);
+    onSubmit(formData.get("search"));
+    formRef.current.reset();
+  };
+  return (
+    <form onSubmit={handleSubmit} ref={formRef}>
+      <input type="text" placeholder="search" name="search" />
+      <button>Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
---
+++ src/services/books-services.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://www.googleapis.com/books/v1/";
+
+export const getBooksBySearchTerm = async (searchTerm) => {
+  if (searchTerm.trim() === "") {
+    throw new Error("Must search for a value");
+  }
+  const response = await fetch(`${BASE_URL}volumes?q=${searchTerm}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch");
+  }
+  const data = await response.json();
+  if (data.totalItems === 0) {
+    throw new Error("No books found with the search term: " + searchTerm);
+  }
+  return data;
+};
